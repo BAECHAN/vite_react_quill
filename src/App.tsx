00b680b2085b5e8b2c66b396569abd2a14010e85
c_bae <c_bae@dorosee.kr>
2024-05-16 17:53:08 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Quill Editor with Video Resize</h1>
+        <h1>Quill Editor with Video Resize!</h1>
       </header>
       <main>
         <MyEditor />
